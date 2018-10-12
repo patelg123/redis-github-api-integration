144a8b7c1e6a916e72bf7586c75df538749d3af4
Girish Patel <patelg123@gmail.com>
2018-10-12 17:24:19 +0100

    changed so that it gets the no of repos a user has

--- server.js
+++ server.js
@@ -9,39 +9,39 @@ const app = express();
 const client = redis.createClient(REDIS_PORT);
 
 
-function respond(org, numberOfRepos) {
-    return `Organization "${org}" has ${numberOfRepos} public repositories.`;
+function respond(user, numberOfRepos) {
+    return `User "${user}" has ${numberOfRepos} public repositories.`;
 }
 
-function getNumberOfOrgsRepos(req, res, next) {
-    const org = req.query.org;
-   request.get(`https://api.github.com/orgs/${org}/repos`, function (err, response) {
+function getNumberOfRepos(req, res, next) {
+    const user = req.query.user;
+   request.get(`https://api.github.com/users/${user}/repos`, function (err, response) {
         if (err) throw err;
 
         // response.body contains an array of public repositories
         var repoNumber = response.body.length;
 
-        client.setex(org, 5, repoNumber);
+        client.setex(user, 5, repoNumber);
 
-        res.send(respond(org, repoNumber));
+        res.send(respond(user, repoNumber));
     });
 };
 
 
 function cache(req, res, next) {
-    const org = req.query.org;
-    client.get(org, function (err, data) {
+    const user = req.query.user;
+    client.get(user, function (err, data) {
         if (err) throw err;
 
         if (data != null) {
-            res.send(respond(org, data));
+            res.send(respond(user, data));
         } else {
             next();
         }
     });
 }
 
-app.get('/orgs/repos', cache, getNumberOfOrgsRepos);
+app.get('/users/repos', cache, getNumberOfRepos);
 
 
 app.listen(PORT, function () {
